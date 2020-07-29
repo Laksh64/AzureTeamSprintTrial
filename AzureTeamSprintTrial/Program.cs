c2204a18b2a4fb5ad732744c5b50f69be6be724f
Laksh64 <Lakshya.Khanna@lntinfotech.com>
2020-07-29 17:29:40 +0530

    Team Sprint

---
+++ AzureTeamSprintTrial/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureTeamSprintTrial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This Project is a trial for Azure Team Sprints Integration.");
+        }
+    }
+}
